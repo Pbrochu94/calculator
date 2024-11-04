664bef66072d4108501adbd84fce67ddfd47fddb
Patrick Brochu <patrickbrochu70@gmail.com>
2024-11-04 11:12:58 -0500

    Added the operator row
    TODO: continue the result function behavior

--- script.js
+++ script.js
@@ -11,8 +11,7 @@ function enterInput(event)
 {
     if(event.target.innerText === "=")
         {
-            console.log(textBox.innerText)
-            textBox.innerText = ''
+            result(textBox.innerText)
             return;
         }
     if(event.target.innerText === "clear")
@@ -23,4 +22,11 @@ function enterInput(event)
     console.log(event.target.innerText);
     textBox.innerText += event.target.innerText;
     textBox.style.color = "rgb(128, 255, 0)";
+}
+
+function result(text)
+{
+    console.log("function start", text)
+    let arr = text.split("[+-/*]")
+    console.log(arr)
 }
