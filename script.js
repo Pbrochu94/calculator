fae4c49d64837550277aa0f7cd8dfb3c159843e7
Patrick Brochu <patrickbrochu70@gmail.com>
2024-11-04 15:53:22 -0500

    Operators behaviors almost completed
    TODO: Need the display to refresh when entering
    new number after total is displayed

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function enterInput(event)
 {
     if(event.target.innerText === "=")
         {
-            result(textBox.innerText)
+            textBox.innerText = result(textBox.innerText)
             return;
         }
     if(event.target.innerText === "clear")
@@ -19,14 +19,44 @@ function enterInput(event)
             textBox.innerText = ''
             return;
         }
-    console.log(event.target.innerText);
     textBox.innerText += event.target.innerText;
     textBox.style.color = "rgb(128, 255, 0)";
 }
 
 function result(text)
 {
-    console.log("function start", text)
-    let arr = text.split("[+-/*]")
-    console.log(arr)
+    let operatorReg = /[+-/x]/g
+    let operators = text.match(operatorReg)
+    let result;
+    if(operators.length >=2)
+    {
+        return "Error"
+    }
+    let arr = text.split(operators)
+    
+    if(operators == '+')
+    {
+        return result = arr.reduce((total, current)=>parseInt(total, 10) + parseInt(current, 10))
+    }    
+    if(operators == "-")
+    {
+        return result = arr.reduce((total, current)=>parseInt(total, 10) - parseInt(current, 10))
+    }
+    if(operators == "x")
+    {
+        return result = arr.reduce((total, current)=>parseInt(total, 10) * parseInt(current, 10))   
+    }
+    if(operators == "/")
+    {        
+        if(arr[1] == "0")
+            {
+                return 0;
+            }  
+        result = parseInt(arr[0]) / parseInt(arr[1])
+        if (parseInt(arr[0]) % parseInt(arr[1]) != 0)
+        {
+            return result.toFixed(2)
+        }
+        return result
+    }
 }
